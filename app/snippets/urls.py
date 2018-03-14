86f0924c35eebb7c3ddfed67d5483a5d53f2fb3a
Shin Dongjin <djshin2000@gmail.com>
2018-03-15 04:30:37 +0900

    ModelSerializer로 수정, 및 view작성

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
 
+from . import views
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
